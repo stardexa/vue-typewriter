8d2a344ee8d73b491d53fce6c8d5e5a43857351c
skmac <suleymankbey@gmail.com>
2022-06-23 01:11:58 +0300

    proje başlangıç
    basic

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# vue-typewriter
+# vue-typewrite-register
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container fs-40" >
+      <span>{{output}}</span>
+      <span class="blink_me">|</span>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "Hello World",
+      output: "",
+      speed: "80",
+      i:0,
+
+    }
+  },
+  mounted() {
+    this.typeWriter()
+  },
+  methods:{
+    typeWriter(){
+      if ( this.i < this.title.length) {
+        this.output += this.title.charAt(this.i);
+        this.i= this.i+1;
+        setTimeout(this.typeWriter, this.speed);
+      }else{
+        setTimeout(this.writeClear,2000)
+      }
+    },
+    writeClear(){
+      this.i = 0
+      this.output = ""
+    }
+  }
+}
+</script>
+<style>
+.fs-40{
+  font-size: 40px;
+}
+.blink_me {
+  animation: blinker 0.9s linear infinite;
+}
+@keyframes blinker {
+  50% { opacity: 0; }
+}
+</style>
