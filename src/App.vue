e02f084c0a0072898c5a97646f75c1874d322381
skmac <suleymankbey@gmail.com>
2022-06-23 02:45:35 +0300

    function ile çalıştırma

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,53 @@
 <template>
-    <div class="container fs-40" >
-      <span>{{output}}</span>
-      <span class="blink_me">|</span>
-    </div>
+  <div class="container fs-40">
+    <button class="btn btn-outline-dark" @click="typeWriterText('İsminizi Giriniz.','name','30')">Name -> TypeWrite</button>
+    <br>
+    <span ref="name"></span>
+    <span class="blink_me">|</span>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: "App",
   data() {
     return {
-      title: "Hello World",
-      output: "",
-      speed: "80",
-      i:0,
-
+      i: 0,
     }
   },
-  mounted() {
-    this.typeWriter()
-  },
-  methods:{
-    typeWriter(){
-      if ( this.i < this.title.length) {
-        this.output += this.title.charAt(this.i);
-        this.i= this.i+1;
-        setTimeout(this.typeWriter, this.speed);
-      }else{
-        setTimeout(this.writeClear,2000)
-      }
-    },
-    writeClear(){
+  methods: {
+    writeClear() {
       this.i = 0
       this.output = ""
-    }
+    },
+    typeWriterText(_title, _for, _speed = 60) {
+      if (this.i < _title.length) {
+        let output = '';
+        output += _title.charAt(this.i);
+        this.i = this.i + 1;
+        this.$refs[_for].innerHTML += output
+        setTimeout(() => {
+          this.typeWriterText(_title, _for, _speed)
+        }, _speed);
+      } else {
+        this.writeClear()
+      }
+    },
   }
 }
 </script>
 <style>
-.fs-40{
+.fs-40 {
   font-size: 40px;
 }
+
 .blink_me {
   animation: blinker 0.9s linear infinite;
 }
+
 @keyframes blinker {
-  50% { opacity: 0; }
+  50% {
+    opacity: 0;
+  }
 }
 </style>
